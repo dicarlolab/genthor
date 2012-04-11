c2f9e43871d1ea2c2067a332d5d9f6bf48edb8d7
Peter Battaglia <pbattaglia@gmail.com>
2012-04-10 22:58:01 -0400

    moved build-database code to separate file

--- renderer/genthor_renderer.py
+++ renderer/genthor_renderer.py
@@ -156,38 +156,3 @@ if __name__ == "__main__":
     #args[1] = ""
     
     lbase = run(args)
-
-
-if False:
-    # Here's the gist of running this thing to produce 10k images:
-    window_type = "offscreen"
-
-    # Set up the renderer
-    lbase, output = setup_renderer(window_type)
-    if window_type == "offscreen":
-        tex = output.getTexture()
-
-    # Fill with the arguments for each img you want rendered.
-    all_args = []
-    
-    Img = np.zeros((len(all_args), 512, 512, 3), "u1")
-
-    for iimg, argss in enumerate(all_args):
-
-        # Construct a scene
-        modelpath, envpth, scale, pos, hpr, phitheta, bgscale = args
-        scenenode = construct_scene(lbase, modelpath, envpth, scale, pos,
-                                    hpr, phitheta, bgscale)
-
-        # Render the scene
-        lbase.render_frame()
-
-        if window_type == "offscreen":
-            # Get the image
-            img = lbase.get_tex_image(tex)
-
-        # Store the img
-        Img[iimg] = img
-
-    # Save out images, or probably do it in the for loop if there are
-    # more than a few hundred.
