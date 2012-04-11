61c531221c8526f061c88d7740c9cca27f5ebc53
Peter Battaglia <pbatt@mit.edu>
2012-04-11 09:40:45 -0400

    reorganization

--- genthor/__init__.py
+++ genthor/__init__.py
@@ -1,8 +1,19 @@
+import os
 import urllib
 import json
 
 BASE_URL = 'http://50.19.109.25'
 MODEL_URL = BASE_URL + ':9999/3dmodels?'
+
+# Root path of your genthor code
+GENTHOR_PATH = os.environ["GENTHOR_PATH"]
+
+# Points to the models
+MODEL_PATH = os.path.join(GENTHOR_PATH, "processed_models")
+
+# Points to the backgrounds
+BACKGROUND_PATH = os.path.join(GENTHOR_PATH, "backgrounds")
+
 
 def get_canonical_view(m):
     v = json.loads(urllib.urlopen(MODEL_URL + 'query={"id":"' + m + '"}&fields=["canonical_view"]').read())[0]
