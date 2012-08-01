2badd904dd58ad7419897ad7451f17ab611d0f78
Dan Yamins <dyamins@gmail.com>
2012-08-01 16:59:29 -0400

    dataset

--- genthor/datasets.py
+++ genthor/datasets.py
@@ -449,6 +449,25 @@ class GenerativeDataset4(GenerativeDatasetBase):
                                [int(round(total * bias[m])) for m in models]))
 
 
+class GenerativeDataset5(GenerativeDataset4):   
+
+    templates = [
+                 {'n_ex_per_model': 250,
+                  'name': 'var1', 
+                  'template': {'bgname': choice(good_backgrounds),
+                     'bgscale': 1.,
+                     'bgpsi': 0,
+                     'bgphi': uniform(-180.0, 180.),
+                     's': uniform(2./3, 3),
+                     'ty': uniform(-0.5, 0.5),
+                     'tz': uniform(-1.0, 1.0),
+                     'ryz': uniform(-180., 180.),
+                     'rxy': uniform(-180., 180.),
+                     'rxz': uniform(-180., 180.),
+                     }
+                  }]   
+
+
 class GenerativeDatasetBoatsVsAll(GenerativeDatasetBase):    
     models = model_info.MODEL_SUBSET_5
     bad_backgrounds = ['INTERIOR_13ST.jpg', 'INTERIOR_12ST.jpg',
