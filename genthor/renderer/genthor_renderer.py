3613c007215717eb4a00fafbcd2b7ef2fea87a13
Dicarlo-Cox <dicarlocox@gmail.com>
2012-08-02 20:37:38 +0000

    bugfix

--- genthor/renderer/genthor_renderer.py
+++ genthor/renderer/genthor_renderer.py
@@ -78,7 +78,8 @@ def construct_scene(lbase, modelpth, bgpath, scale, pos, hpr, bgscale, bghp):
     # Modelpth points to the model .egg/.bam file
     objnode = read_file(lbase.loader.loadModel, modelpth)
     objnode.setScale(scale[0], scale[0], scale[0])
-    objnode.setPos(pos[0], pos[1], 0.)
+    #objnode.setPos(pos[0], pos[1], 0.)
+    objnode.setPos(pos[0], 0., pos[1])
     objnode.setHpr(hpr[0], hpr[1], hpr[2])
     objnode.setTwoSided(1)
 
