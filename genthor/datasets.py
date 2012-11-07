1962f523c048a7175970f236cee7087322f460aa
Ethan Solomon <esolomon@mit.edu>
2012-11-07 14:12:24 -0500

    updates to CanonicalBase addressing Dan's comments

--- genthor/datasets.py
+++ genthor/datasets.py
@@ -129,31 +129,36 @@ class GenerativeBase(DatasetBase):
 class CanonicalBase(GenerativeBase):
 	#Subclass to save canonical scale/pose in a database.
 	#Runs get_image and saves parameters in database each time.
-	
-	def __init__(self, dbname='canonicalviews', colname='handset', port=27017):
-		GenerativeBase.__init__(self, data=None)
-		self.dbname = dbname
-		self.colname = colname
-		#self.hostname = hostname
-		self.port = port
-		self.conn = pymongo.Connection(port=self.port) #Open connection
-		self.db = self.conn[self.dbname]
-		self.col = self.db[self.colname]
-			
-	def saveCanonical(self, preproc, config):
-		#Checks to see that config contains necessary fields. Increments all prior versions of obj
-		#submitted by user by 1, and adds new database entry with version = 0. Returns rendered image.
-		keys_passed = config.keys()
-		if ('user' in keys_passed) and ('obj' in keys_passed) and ('version' in keys_passed):
-			self.col.update({'obj':config['obj'], 'user':config['user']}, {'$inc': {'version':1}}, multi=True)
-			self.col.update({'obj': config['obj'],'user':config['user'],'version':0}, config, upsert=True)		
-			return self.get_image(preproc, config)
-		else:
-			raise Exception('Parameters must include "user", "obj", and "version" fields')
-		
-	def getCanonical(self,obj,user):
-		#Returns most recent database entry to match query, if it exists.
-		list(self.col.find({'obj':obj,'user':user,'version':0}))
+
+    def __init__(self, dbname='canonicalviews', colname='handset', hostname='localhost', port=27017):
+        GenerativeBase.__init__(self, data=None)
+        self.dbname = dbname
+        self.colname = colname
+        self.hostname = hostname
+        self.port = port
+        self.conn = pymongo.Connection(port=self.port, host=self.hostname) #Open connection
+        self.db = self.conn[self.dbname]
+        self.col = self.db[self.colname]
+        self.col.ensure_index([('obj', pymongo.ASCENDING),
+                                 ('user', pymongo.ASCENDING),
+                                 ('version', pymongo.DESCENDING)],
+                                unique=True)
+
+    def saveCanonical(self, preproc, config):
+        #Checks to see that config contains necessary fields. Increments all prior versions of obj
+        #submitted by user by 1, and adds new database entry with version = 0. Returns rendered image.
+        keys_passed = config.keys()
+        if ('user' in keys_passed) and ('obj' in keys_passed):
+            self.col.update({'obj' : config['obj'], 'user' : config['user']}, {'$inc' : {'version' : 1}}, multi=True, safe=True)
+            config['version'] = 0
+            self.col.insert(config, safe=True)
+            return self.get_image(preproc, config)
+        else:
+            raise Exception('Parameters must include "user" and "obj" fields')
+
+    def getCanonical(self,obj,user,version=0):
+        #Returns most recent database entry to match query, if it exists.
+        return self.col.find_one({'obj' : obj, 'user' : user, 'version' : version})
 
 
 class GenerativeDatasetBase(GenerativeBase):
@@ -180,7 +185,7 @@ class GenerativeDatasetBase(GenerativeBase):
             else:
                 n_ex_dict = dict([(m, tdict['n_ex_per_model']) for m in models])
             for model in models:
-                #print('Generating meta for %s' % model)
+                print('Generating meta for %s' % model)
                 for _ind in range(n_ex_dict[model]):
                     l = stochastic.sample(template, rng)
                     l['obj'] = model
